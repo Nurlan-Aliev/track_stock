9c414dc5f3062f5281298d393743b7e093eda18f
Aliev Nurlan <nualiev420@gmail.com>
2023-08-10 00:09:17 +0400

    setting

--- goods_map/settings.py
+++ goods_map/settings.py
@@ -148,4 +148,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['trackstock-production.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://trackstock-production.up.railway.app/']
