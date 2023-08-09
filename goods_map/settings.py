28cc1d0263150aef60b2005c93badee9e69b7b5e
Aliev Nurlan <nualiev420@gmail.com>
2023-08-09 23:07:54 +0400

    ref settings

--- goods_map/settings.py
+++ goods_map/settings.py
@@ -34,11 +34,9 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    'https://trackstock-production.up.railway.app/'
 ]
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
