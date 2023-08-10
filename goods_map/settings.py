b0fe8fe3387286431fb7c586debdf88cf21d0c26
Aliev Nurlan <nualiev420@gmail.com>
2023-08-10 12:06:33 +0400

    fix image

--- goods_map/settings.py
+++ goods_map/settings.py
@@ -142,6 +142,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
