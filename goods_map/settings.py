2b7cb8b9cacb7b25dc5ada6dc17930916b1131c3
Aliev Nurlan <nualiev420@gmail.com>
2023-08-10 08:32:27 +0400

    image

--- goods_map/settings.py
+++ goods_map/settings.py
@@ -134,7 +134,7 @@ USE_I18N = True
 USE_TZ = True
 
 LOGIN_URL = reverse_lazy('login')
-MEDIA_URL = '/media/'
+MEDIA_URL = os.path.join(BASE_DIR, 'media/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
