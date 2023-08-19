caa4f16a06a0da072ed5e00fa9fa4791a438a525
Aliev Nurlan <nualiev420@gmail.com>
2023-08-20 01:06:03 +0400

    stock ref

--- goods_map/stock/views.py
+++ goods_map/stock/views.py
@@ -1,20 +1,25 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView
 from goods_map.stock.forms import StockForm, MarketForm
 from goods_map.stock.models import StockModel, MarketModel
 from django.urls import reverse_lazy
 
 
-class StockHome(ListView):
+class StockHome(LoginRequiredMixin, ListView):
     template_name = 'stock.html'
     model = StockModel
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['market'] = MarketModel.objects.all()
+        return context
 
 
 class CreateStock(LoginRequiredMixin, CreateView):
     template_name = 'edit.html'
     model = StockModel
     form_class = StockForm
-    success_url = reverse_lazy('good_list')
+    success_url = reverse_lazy('stock_home')
     extra_context = {'title': 'Create stock place', 'button': 'Create'}
 
 
@@ -22,5 +27,5 @@ class CreateMarket(LoginRequiredMixin, CreateView):
     template_name = 'edit.html'
     model = MarketModel
     form_class = MarketForm
-    success_url = reverse_lazy('good_list')
+    success_url = reverse_lazy('stock_home')
     extra_context = {'title': 'Create market place', 'button': 'Create'}
